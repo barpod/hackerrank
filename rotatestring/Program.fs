409aad6fb26522b1cdd2a780b0c21c30ab29dcfe
Bartosz Podlejski <bartosz.podlejski@trapezegroup.pl>
2018-05-22 08:39:57 +0200

    Solution for https://www.hackerrank.com/challenges/rotate-string/problem

---
+++ rotatestring/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+let n = Console.ReadLine() |> int
+
+let last n xs = xs |> Seq.skip (Seq.length xs - n)
+
+let rotate n s = Seq.append (last (Seq.length s - n) s) (Seq.take n s) |> Seq.map string |> String.concat "" 
+
+let read_and_rotate_string () =
+    let s = Console.ReadLine()
+    [1..s.Length] |> List.map (fun n -> rotate n s)
+
+[1..n] 
+    |> List.map (fun _ -> read_and_rotate_string ()) 
+    |> List.map (fun a -> String.concat " " a) 
+    |> List.iter (fun s -> printfn "%s" s)
